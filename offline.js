4a32a1765960655601240498ba495d716db0c783
sahabia <sahabia-games@outlook.com>
2022-02-22 11:51:07 +0330

    upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1645516608,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/09u09u-sheet0.png",
+		"images/09u09u3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js",
+		"penumbra.png"
+	]
+}
